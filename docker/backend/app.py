c360f182bb78aeec8e0b5507f5920908f4166d46
Ab1las7 <abilashbm7@gmail.com>
2026-04-22 00:59:50 +0530

    adding docker content

---
+++ docker/backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask jsonify
+import redis    
+import os
+
+app = Flask(__name__)
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+r = redis.Redis(host=redis_host, port=6379)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Backend running"})
+
+@app.route("/api")
+def api():
+    count = r.incr("hits")
+    return jsonify({"hits": count})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0" , port=5000)        
